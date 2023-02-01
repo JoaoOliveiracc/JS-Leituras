ba25736aa67ae4e6e720a6f720ca7c3f35a45e34
JoaoOliveiracc <ccjoao_monteiro@outlook.com>
2023-02-01 17:36:53 -0300

    Variáveis booleanas

--- elderFlower/js/string-variable.js
+++ elderFlower/js/string-variable.js
@@ -5,5 +5,6 @@ message = 'See our upcoming range';
 
 var elName = document.getElementById('name');
 elName.textContent = username;
+
 var elNote = document.getElementById('note');
 elNote.textContent = message;
--- elderFlower/js/string-with-quotes.js
+++ elderFlower/js/string-with-quotes.js
@@ -5,5 +5,6 @@ message = '<a href=\"sale.html\">25% off!</a>';
 
 var elTitle = document.getElementById('title');
 elTitle.innerHTML = title;
+
 var elNote = document.getElementById('note');
 elNote.innerHTML = message;
