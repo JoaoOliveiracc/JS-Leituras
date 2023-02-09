8fffa9c9439052f4a621890d49c7ed7c45116ea0
JoaoOliveiracc <ccjoao_monteiro@outlook.com>
2023-02-09 14:16:33 -0300

    add html and js settings

--- c03/js/object-constructor.js
+++ c03/js/object-constructor.js
@@ -7,7 +7,7 @@ hotel.checkAvailability = function() {
     return this.rooms - this.booked;
 };
 
-var elName = document.getElementById('name');
+var elName = document.getElementById('hotelName');
 elName.textContent = hotel.name;
 
 var elRooms = document.getElementById('rooms');
